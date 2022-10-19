5acd22b8c2f65f0d6d4489c092a293fba02adbc9
Mateus <111003525+neomateus@users.noreply.github.com>
2022-10-19 19:01:01 +0000

    .

--- Aula6/main.cs
+++ Aula6/main.cs
@@ -1,36 +1,33 @@
 using Modelos;
 
-class Aula5 {
+class Aula6 {
 
   public static void Main (string[] args) {
     Aluno aluno1 = new Aluno(0);
-    aluno1.Cpf = "206.523.670-11";
+    aluno1.Cpf = "422.657.877-99";
     Impressora.print(aluno1);
-    
     Aluno aluno2 = new Aluno(1);
-    aluno2.Cpf = "759.187.350-46";
+    aluno2.Cpf = "660.036.460-20";
     Impressora.print(aluno2);
     
     Pessoa pessoa1 = new Professor();
-    pessoa1.Cpf = "750.029.400-05";
-    Impressora.print(pessoa1);
-    
+    //pessoa1.Cpf = "588.401.710-37";
+    //Impressora.print(pessoa1);
     Pessoa pessoa2 = (Pessoa) aluno2;
     Impressora.print(pessoa2);
     
     Aluno aluno3 = (Aluno) pessoa2;
     Impressora.print(aluno3);
-    
     Professor professor1 = new Professor();
-    professor1.Cpf = "869.766.640-68";
+    professor1.Cpf = "851.152.090-20";
     Impressora.print(professor1);
 
-    Familiar familiar1 =  new Familiar();
-    familiar1.Nome = "Roberto Cristovão";
-    familiar1.Cpf = "230.097.040-62";
-    familiar1.Parentesco = "Tio";
+    Familiar familiar1 = new Familiar();
+    familiar1.Nome = "Rafaela Shimidth";
+    familiar1.Cpf = "446.637.010-94";
+    familiar1.Parentesco = "Mãe";
     Impressora.print(familiar1);
+    
   }
 
 }
-
